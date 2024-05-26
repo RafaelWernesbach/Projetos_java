cf8ca074805a4341f3803763b8e57c7b551c9ea2
Rafael Christian Silva Wernesbach <126472411+RafaelWernesbach@users.noreply.github.com>
2024-05-25 22:14:10 -0300

    Update Espada.java

--- Implementações/Trabalhob2_1/armas/Espada.java
+++ Implementações/Trabalhob2_1/armas/Espada.java
@@ -1,1 +1,25 @@
 
+package armas;
+
+import personagens.Personagem;
+
+
+public class Espada implements Arma_IF {
+
+    @Override
+    public void usar(Personagem alvo) {
+        if (alvo.estaVivo()) {
+            System.out.println("Atacando com a espada!");
+            int danoCausado = causarDano();
+            alvo.receberDano(danoCausado);
+        } else {
+            System.out.println("Não é possível atacar um personagem morto!");
+        }   
+    }
+
+    @Override
+    public int causarDano() {
+        return 120;
+    }
+    
+}
