8a1cbb7a0cc1253e9a41670915f9e1244a7555c7
Rafael Wernesbach <126472411+RafaelWernesbach@users.noreply.github.com>
2024-06-15 13:28:56 -0300

    Update Dispositivos.java

--- Implementações/bridge/Dispositivos.java
+++ Implementações/bridge/Dispositivos.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package bridge;
 
-/**
- *
- * @author BRAIA
- */
 public interface Dispositivos {
     String getNome();
     boolean setPower();
