89af79e17b2fb990e0a6827ba5fb728a8bb37afe
Rafael Wernesbach <126472411+RafaelWernesbach@users.noreply.github.com>
2024-06-15 13:28:14 -0300

    Update Televisao.java

--- Implementações/bridge/Televisao.java
+++ Implementações/bridge/Televisao.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package bridge;
 
-/**
- *
- * @author BRAIA
- */
+
 public class Televisao implements Dispositivos{
     String nome;
     boolean power = true;
